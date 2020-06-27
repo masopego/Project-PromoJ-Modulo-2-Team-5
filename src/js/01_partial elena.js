2303266f67fb2257f5fed22b7787edfae8bbc86a
erreinoso <eramirezreinoso@gmail.com>
2020-06-27 15:40:53 +0200

    before Elena push

--- src/js/01_partial elena.js
+++ src/js/01_partial elena.js
@@ -5,7 +5,7 @@ const userName = document.querySelector('.js-inputName');
 const userJob = document.querySelector('.js-inputJob');
 // const userEmail = document.querySelector('.js-inputEmail');
 // const userPhone = document.querySelector('.js-phone');
-const userLinkedin = document.querySelector('.js-linkedin');
+// const userLinkedin = document.querySelector('.js-linkedin');
 // const userGithub = document.querySelector('.js-github');
 
 //constantes de los lugares donde renderizaremos
@@ -14,7 +14,7 @@ const renderUserName = document.querySelector('.js-namePreview');
 const renderUserJob = document.querySelector('.js-jobPreview');
 // const renderUserEmail = document.querySelector('.js-phonePreview');
 // const renderUserPhone = document.querySelector('.js-emailPreview');
-const renderUserLinkedin = document.querySelector('.js-linkedinPreview');
+// const renderUserLinkedin = document.querySelector('.js-linkedinPreview');
 // const renderUserGithub = document.querySelector('.js-githubPreview');
 
 const userData = {}; //llenarea la funcion vacía con los datos
@@ -24,16 +24,15 @@ function getInputValues() {
   userData.job = userJob.value;
   // userData.email = userEmail.value;
   // userData.phone = userPhone.value;
-  userData.linkedin = userLinkedin.value;
+  // userData.linkedin = userLinkedin.value;
   // userData.github = userGithub.value;
 }
 
 function renderUserInfo(data) {
   renderUserName.innerHTML = data.name;
-  renderUserJob.innerHTML = data.job;
-  // renderUserEmail.innerHTML = data.email;
+  renderUserJob.innerHTML = data.job; // renderUserEmail.innerHTML = data.email;
   // renderUserPhone.innerHTML = data.phone;
-  renderUserLinkedin.innerHTML = data.linkedin;
+  // renderUserLinkedin.innerHTML = data.linkedin;
   // renderUserGithub.innerHTML = data.github;
   if (renderUserName.innerHTML === '') {
     renderUserName.innerHTML = 'Jeanne Baret';
@@ -41,6 +40,7 @@ function renderUserInfo(data) {
   if (renderUserJob.innerHTML === '') {
     renderUserJob.innerHTML = 'Botanist';
   }
+  
 } //pinta la propiedad de ese objeto que se llame name,job..
 
 function updateInputHandler() {
@@ -52,5 +52,5 @@ userName.addEventListener('keyup', updateInputHandler);
 userJob.addEventListener('keyup', updateInputHandler);
 // userEmail.addEventListener('keyup', updateInputHandler);
 // userPhone.addEventListener('keyup', updateInputHandler);
-userLinkedin.addEventListener('keyup', updateInputHandler);
+// userLinkedin.addEventListener('keyup', updateInputHandler);
 // userGithub.addEventListener('keyup', updateInputHandler);
