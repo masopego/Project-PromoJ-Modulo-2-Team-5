11d109e6704d5646899c0904e69aaa6730845828
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-06-27 12:05:01 +0200

    Change Js class names

--- src/js/01_partial_Sonia.js
+++ src/js/01_partial_Sonia.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const borderPV = document.querySelector('.border-js');
-const namePV = document.querySelector('.name-js');
-const iconPV = document.querySelector('.icon-js');
-const framePV = document.querySelector('.frame-js');
-const photoPV = document.querySelector('.photo-js');
+const borderPV = document.querySelector('.js-border');
+const namePV = document.querySelector('.js-name');
+const iconPV = document.querySelector('.js-icon');
+const framePV = document.querySelector('.js-frame');
+const photoPV = document.querySelector('.js-photo');
 
 const coldSelector = document.querySelector('.cold');
 const warmSelector = document.querySelector('.warm');
@@ -26,12 +26,12 @@ function changeToCold() {
   iconPV.classList.remove('icon-warm', 'icon-medium');
 
   //FRAME
-  framePV.classList.add('frame-cold');
-  framePV.classList.remove('frame-warm', 'frame-medium');
+  framePV.classList.add('js-frame-cold');
+  framePV.classList.remove('js-frame-warm', 'js-frame-medium');
 
   //PHOTO
   photoPV.classList.add('photo-cold');
-  photoPV.classList.remove('photo-warm', 'photo-medium', 'photo-js-default');
+  photoPV.classList.remove('photo-warm', 'photo-medium', 'js-photo-default');
 }
 
 coldSelector.addEventListener('change', changeToCold);
@@ -52,12 +52,12 @@ function changeToWarm() {
   iconPV.classList.remove('icon-cold', 'icon-medium');
 
   //FRAME
-  framePV.classList.add('frame-warm');
-  framePV.classList.remove('frame-cold', 'frame-medium');
+  framePV.classList.add('js-frame-warm');
+  framePV.classList.remove('js-frame-cold', 'js-frame-medium');
 
   //PHOTO
   photoPV.classList.add('photo-warm');
-  photoPV.classList.remove('photo-cold', 'photo-medium', 'photo-js-default');
+  photoPV.classList.remove('photo-cold', 'photo-medium', 'js-photo-default');
 }
 
 warmSelector.addEventListener('change', changeToWarm);
@@ -78,22 +78,22 @@ function changeToMedium() {
   iconPV.classList.remove('icon-cold', 'icon-warm');
 
   //FRAME
-  framePV.classList.add('frame-medium');
-  framePV.classList.remove('frame-cold', 'frame-warm');
+  framePV.classList.add('js-frame-medium');
+  framePV.classList.remove('js-frame-cold', 'js-frame-warm');
 
   //PHOTO
   photoPV.classList.add('photo-medium');
-  photoPV.classList.remove('photo-cold', 'photo-warm', 'photo-js-default');
+  photoPV.classList.remove('photo-cold', 'photo-warm', 'js-photo-default');
 }
 
 mediumSelector.addEventListener('change', changeToMedium);
 
 const frameSwitcher = document.querySelector('.js-frame-button');
 
-const frameHidden = document.querySelector('.frame-hidden-js');
+const frameHidden = document.querySelector('.js-frame-hidden');
 
 function switchFrame() {
-  frameHidden.classList.toggle('frame-hidden-js');
+  frameHidden.classList.toggle('js-frame-hidden');
   frameSwitcher.classList.toggle('js-frame-button-off');
   frameSwitcher.classList.toggle('js-frame-button-active');
 }
