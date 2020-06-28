5da8825c13e39a73d203a8802ed1f4ce66922482
VirginiaGomezR <virginiagomezr@gmail.com>
2020-06-28 18:08:33 +0200

    class name fixed

--- src/js/04_partial_vir.js
+++ src/js/04_partial_vir.js
@@ -9,74 +9,74 @@ const usergithub = document.querySelector('#github');
 
 function validationUserName() {
   if (username.value === '') {
-    username.classList.add('border-input');
-    username.parentElement.classList.add('error');
+    username.classList.add('js-border-input');
+    username.parentElement.classList.add('js-error');
   } else {
-    username.classList.remove('border-input');
-    username.parentElement.classList.remove('error');
+    username.classList.remove('js-border-input');
+    username.parentElement.classList.remove('js-error');
   }
 }
 
 function validationJob() {
   if (userjob.value === '') {
-    userjob.classList.add('border-input');
-    userjob.parentElement.classList.add('error');
+    userjob.classList.add('js-border-input');
+    userjob.parentElement.classList.add('js-error');
   } else {
-    userjob.classList.remove('border-input');
-    userjob.parentElement.classList.remove('error');
+    userjob.classList.remove('js-border-input');
+    userjob.parentElement.classList.remove('js-error');
   }
 }
 
 // /*if (userimage.value === '') {
-//   userimage.classList.add('border-input');
+//   userimage.classList.add('js-border-input');
 // } else {
-//   userimage.classList.remove('border-input');
+//   userimage.classList.remove('js-border-input');
 // }*/
 
 function validationEmail() {
   if (useremail.value === '' || !useremail.value.includes('@')) {
-    useremail.classList.add('border-input');
-    useremail.parentElement.classList.add('error');
+    useremail.classList.add('js-border-input');
+    useremail.parentElement.classList.add('js-error');
   } else {
-    useremail.classList.remove('border-input');
-    useremail.parentElement.classList.remove('error');
+    useremail.classList.remove('js-border-input');
+    useremail.parentElement.classList.remove('js-error');
   }
 }
 
 function validationPhone() {
   if (userphone.value === '') {
-    userphone.classList.add('border-input');
-    userphone.parentElement.classList.add('error');
+    userphone.classList.add('js-border-input');
+    userphone.parentElement.classList.add('js-error');
   } else {
-    userphone.classList.remove('border-input');
-    userphone.parentElement.classList.remove('error');
+    userphone.classList.remove('js-border-input');
+    userphone.parentElement.classList.remove('js-error');
   }
 }
 
 function validationLinkedin() {
   if (userlinkedin.value === '') {
-    userlinkedin.classList.add('border-input');
-    userlinkedin.parentElement.classList.add('error');
+    userlinkedin.classList.add('js-border-input');
+    userlinkedin.parentElement.classList.add('js-error');
   } else {
-    userlinkedin.classList.remove('border-input');
-    userlinkedin.parentElement.classList.remove('error');
+    userlinkedin.classList.remove('js-border-input');
+    userlinkedin.parentElement.classList.remove('js-error');
   }
 }
 
 function validationGithub() {
   if (usergithub.value === '') {
-    usergithub.classList.add('border-input');
-    usergithub.parentElement.classList.add('error');
+    usergithub.classList.add('js-border-input');
+    usergithub.parentElement.classList.add('js-error');
   } else {
-    usergithub.classList.remove('border-input');
-    usergithub.parentElement.classList.remove('error');
+    usergithub.classList.remove('js-border-input');
+    usergithub.parentElement.classList.remove('js-error');
   }
 }
 
-username.addEventListener('change', validationUserName);
-userjob.addEventListener('change', validationJob);
+username.addEventListener('keyup', validationUserName);
+userjob.addEventListener('keyup', validationJob);
 //userimage.addEventListener('change', validationUserName);
-useremail.addEventListener('change', validationEmail);
-userphone.addEventListener('change', validationPhone);
-userlinkedin.addEventListener('change', validationLinkedin);
-usergithub.addEventListener('change', validationGithub);
+useremail.addEventListener('keyup', validationEmail);
+userphone.addEventListener('keyup', validationPhone);
+userlinkedin.addEventListener('keyup', validationLinkedin);
+usergithub.addEventListener('keyup', validationGithub);
